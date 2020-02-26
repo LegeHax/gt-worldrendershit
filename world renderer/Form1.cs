e7354fa998b90f213e0570a0591e0e8b58474d49
LegeHax <58985510+LegeHax@users.noreply.github.com>
2020-02-26 09:28:36 +0200

    Add files via upload

---
+++ world renderer/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace world_renderer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var request = WebRequest.Create("https://s3.amazonaws.com/world.growtopiagame.com/" + textBox1.Text + ".png");
+                using (var response = request.GetResponse())
+                using (var stream = response.GetResponseStream())
+                {
+                    pictureBox1.Image = Bitmap.FromStream(stream);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("", "error");
+            }
+        }
+    }
+}
